537fc298e6ebc6483be78c3977ee6f4afdd21452
Dieter Luypaert <dieterluypaert@gmail.com>
2019-06-21 14:44:52 +0200

    Sum = 'I' + 'IV': Failing

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 
 exports.add = function add(num1, num2) {
-  return (num1+num2)
-  .replace('IIIII','V')
-  .replace('IIII','IV')
+  return (num1 + num2)
+    .replace('IIIII','V')
+    .replace('IIII','IV')
   ;
 }
--- src/index.spec.js
+++ src/index.spec.js
@@ -2,16 +2,20 @@ const {add} = require('./');
 
 it('should add I and I', () => {
   expect(add('I', 'I')).toBe('II');
-})
+});
 
 it('should add I and II', () => {
   expect(add('I', 'II')).toBe('III');
-})
+});
 
 it('should add I and III', () => {
   expect(add('I', 'III')).toBe('IV');
-})
+});
 
 it('should add II and III', () => {
   expect(add('II', 'III')).toBe('V');
-})
+});
+
+it('should add I and IV', () => {
+  expect(add('I', 'IV')).toBe('V');
+});
