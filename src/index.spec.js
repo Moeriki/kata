939284236ca91e878a480871f992f2b16a414341
Dieter Luypaert <dieterluypaert@gmail.com>
2019-06-21 14:24:27 +0200

    Sum = 'III': Failing

--- src/index.spec.js
+++ src/index.spec.js
@@ -3,3 +3,7 @@ const {add} = require('./');
 it('should add I and I', () => {
   expect(add('I', 'I')).toBe('II');
 })
+
+it('should add I and II', () => {
+  expect(add('I', 'II')).toBe('III');
+})
