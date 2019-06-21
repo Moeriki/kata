460ad2783924375079f1cb13147e4c9d31212a62
Dieter Luypaert <dieterluypaert@gmail.com>
2019-06-21 14:57:08 +0200

    Commutativity tested: Refactored

--- src/index.spec.js
+++ src/index.spec.js
@@ -6,16 +6,20 @@ it('should add I and I', () => {
 
 it('should add I and II', () => {
   expect(add('I', 'II')).toBe('III');
+  expect(add('II', 'I')).toBe('III');
 });
 
 it('should add I and III', () => {
   expect(add('I', 'III')).toBe('IV');
+  expect(add('III', 'I')).toBe('IV');
 });
 
 it('should add II and III', () => {
   expect(add('II', 'III')).toBe('V');
+  expect(add('III', 'II')).toBe('V');
 });
 
 it('should add I and IV', () => {
   expect(add('I', 'IV')).toBe('V');
+  expect(add('IV', 'I')).toBe('V');
 });
