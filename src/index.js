5c5330a2c608d45acbc70675ccb146754327b427
Dieter Luypaert <dieterluypaert@gmail.com>
2019-07-05 14:38:43 +0200

    WIP

--- src/index.js
+++ src/index.js
@@ -1,10 +1,37 @@
+const numerals = [
+  {
+    numeral: 'I'
+  },
+  {
+    numeral: 'V',
+    prefixNumeral: 'I',
+    deconstructed:'IIIII'
+  },
+  {
+    numeral: 'X',
+    prefixNumeral: 'I',
+    deconstructed:'IIIIIIIIII'
+  }
+];
+function deconstruct(num){
+  return numerals
+}
+
+function construct(num){
+
+}
 
 exports.add = function add(num1, num2) {
-  return (num1 + num2)
-    .replace('IV','IIII')
-    .replace('IIIII','V')
-    .replace('IIII','IV')
-  ;
+  // return (num1 + num2)
+  //   .replace('IV','IIII')
+  //   .replace('IIIII','V')
+  //   .replace('IIII','IV');
+    const deconstructedNum1 = deconstruct(num1);
+    const deconstructedNum2 = deconstruct(num2);
 
-  //
+    return construct(deconstructedNum1 + deconstructedNum2);
 }
+
+
+// 99 = XCIX
+  // 8 = IIX
